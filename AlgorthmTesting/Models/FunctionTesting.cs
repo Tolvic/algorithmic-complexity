0b0e1f4467da834f15cf75901d5a45e54a90a848
Tolvic <terence.h.mace@gmail.com>
2018-12-03 21:57:57 +0000

    refactored methods

--- AlgorthmTesting/Models/FunctionTesting.cs
+++ AlgorthmTesting/Models/FunctionTesting.cs
@@ -14,40 +14,19 @@ namespace AlgorthmTesting.Models
             int counter = 0;
             for (int i = startArraySize; i < maxArraySize; i+= incrementSize)
             {
-                if (!results.ContainsKey(i))
-                {
-                    results.Add(i, new double[1]); // change length when outer loop introduced
-                }
-                int[] testArray = Setup(i);
-                Stopwatch stopWatch = Stopwatch.StartNew();
-                functionToBeTested(testArray);
-                stopWatch.Stop();
+                if (!results.ContainsKey(i)) results.Add(i, new double[1]); // change length when outer loop introduced
 
-                Console.WriteLine(functionToBeTested(testArray));
+                int[] testArray = CreateShuffledArray(i);
 
-                int[] rs = functionToBeTested(testArray);
-                Console.WriteLine($"This is loop {i}");
-                foreach (int item in rs)
-                {
-                    Console.WriteLine(item);
-                }
+                results[i][0] = FunctionExecutionTimer(functionToBeTested, testArray); //changle array reference when outer loop introduced
 
-
-
-
-
-
-                TimeSpan ts = stopWatch.Elapsed;
-                var result = ts.TotalMilliseconds;
-      
-                results[i][0] = result; //changle array reference when outer loop introduced
                 counter++;
             }
 
             return results;
         }
 
-
+        // Just for dummy testing SpeedTest function
         public static int[] TestFuction(int[] arr)
         {
             return arr;
@@ -55,20 +34,29 @@ namespace AlgorthmTesting.Models
 
 
 
-        private static int[] CreateArrayOfSequencialValues(int i)
+        private static int[] CreateArrayOfSequentialValues(int i)
         {
             return Enumerable.Range(1, i).ToArray();
         }
 
 
-        private static int[] Setup(int i)
+        private static int[] CreateShuffledArray(int i)
         {
-            int[] arr = CreateArrayOfSequencialValues(i);
+            int[] arr = CreateArrayOfSequentialValues(i);
          
             new Random().Shuffle(arr);
 
             return arr;
         }
 
+        private static Double FunctionExecutionTimer(Func<int[], int[]> functionToBeTested, int[] testArray)
+        {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            functionToBeTested(testArray);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            return ts.TotalMilliseconds;
+        }
+      
     }
 }
