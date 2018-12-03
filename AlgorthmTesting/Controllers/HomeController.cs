748ac6d5c0b277b42a0342c415a45f32d8dab3d6
Tolvic <terence.h.mace@gmail.com>
2018-12-03 21:40:32 +0000

    created MVC infastructure

---
+++ AlgorthmTesting/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AlgorthmTesting.Models;
+
+namespace AlgorthmTesting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var result = FunctionTesting.SpeedTest(FunctionTesting.TestFuction, 1, 10, 1);
+            ViewBag.result = result;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AlgorthmTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlgorthmTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
