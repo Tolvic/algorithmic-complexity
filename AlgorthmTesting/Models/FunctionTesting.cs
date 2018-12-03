d1d6b8bda06185d78f1ad1c53b0ef328ceb842b8
Tolvic <terence.h.mace@gmail.com>
2018-12-03 23:11:55 +0000

    refactored code

--- AlgorthmTesting/Models/FunctionTesting.cs
+++ AlgorthmTesting/Models/FunctionTesting.cs
@@ -11,51 +11,32 @@ namespace AlgorthmTesting.Models
         public static IDictionary<int, double> SpeedTest(Func<int[],int[]> functionToBeTested, int startArraySize, int maxArraySize, int incrementSize, int NumberTimesTestsRepeated)
         {
             IDictionary<int, double[]> results = new Dictionary<int, double[]>();
-            //int counter = 0;
-
 
             for (int n = 0; n < NumberTimesTestsRepeated; n++) 
             {
                 for (int i = startArraySize; i <= maxArraySize; i += incrementSize)
                 {
-                    if (!results.ContainsKey(i)) results.Add(i, new double[NumberTimesTestsRepeated]); // change length when outer loop introduced
+                    if (!results.ContainsKey(i)) results.Add(i, new double[NumberTimesTestsRepeated]); // on first iteration adds an empty array to the results dictionary
 
                     int[] testArray = CreateShuffledArray(i);
 
-                    results[i][n] = FunctionExecutionTimer(functionToBeTested, testArray); //changle array reference when outer loop introduced
-
-                    //counter++;
+                    results[i][n] = FunctionExecutionTimer(functionToBeTested, testArray);
                 }
 
             }
 
-            IDictionary<int, double> averageResults = new Dictionary<int, double>();
-            foreach (var item in results)
-            {
-                double sum = 0;
-
-                for (int i = 0; i < item.Value.Length; i++)
-                {
-                    sum += item.Value[i];
-                }
-                averageResults[item.Key] = sum / item.Value.Length;
-            }
-
-            return averageResults;
+            return CalculateAverages(results);
         }
 
         // Just for dummy testing SpeedTest function
         public static int[] TestFuction(int[] arr)
         {
-
             foreach(int value in arr)
             {
                 Console.WriteLine(value);
             }
             return arr;
         }
-
-
 
         private static int[] CreateArrayOfSequentialValues(int i)
         {
@@ -80,6 +61,23 @@ namespace AlgorthmTesting.Models
             TimeSpan ts = stopWatch.Elapsed;
             return ts.TotalMilliseconds * 1000000;
         }
-      
+
+        private static IDictionary<int, double> CalculateAverages(IDictionary<int, double[]> results)
+        {
+            IDictionary<int, double> averageResults = new Dictionary<int, double>();
+            foreach (var item in results)
+            {
+                double sum = 0;
+
+                for (int i = 0; i < item.Value.Length; i++)
+                {
+                    sum += item.Value[i];
+                }
+                averageResults[item.Key] = sum / item.Value.Length;
+            }
+
+            return averageResults;
+        }
+
     }
 }
