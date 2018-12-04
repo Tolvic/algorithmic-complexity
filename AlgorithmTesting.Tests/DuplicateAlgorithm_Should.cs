f86f06988616ef256635725f8536b26536aac1ef
Tolvic <terence.h.mace@gmail.com>
2018-12-04 16:27:44 +0000

    created a duplicate algorithm

--- AlgorithmTesting.Tests/DuplicateAlgorithm_Should.cs
+++ AlgorithmTesting.Tests/DuplicateAlgorithm_Should.cs
@@ -1,8 +1,10 @@
 using System;
 using NUnit.Framework;
 using AlgorithmTesting.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace AlgorithmTesting.UnitTests.MethodExtensions
+namespace AlgorithmTesting.UnitTests
 {
     public class Tests
     {
@@ -14,8 +16,9 @@ namespace AlgorithmTesting.UnitTests.MethodExtensions
         public void Duplicate_ReturnsArrayOfDuplicates()
         {
             int[] array = { 1, 2, 2, 4, 3, 6, 7, 3, 4, 5, 6, 2, 4 };
-            int[] result = OurDuplicateMethod(array);
+            int[] result = DuplicateAlgorithm.TheBestDuplicateAlgorithm(array);
             Array.Sort(result);
+            result.ToList().ForEach(Console.WriteLine);
             int[] expected = { 2, 3, 4, 6 };
             Assert.That(result, Is.EqualTo(expected));
         }
--- AlgorithmTesting/Models/DuplicateAlgorithm.cs
+++ AlgorithmTesting/Models/DuplicateAlgorithm.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AlgorithmTesting.Models
 {
     public class DuplicateAlgorithm
     {
+        public static int[] TheBestDuplicateAlgorithm(int[] input)
+        {
+            List<int> result = new List<int>();
 
+            int [] temp = new int[input.Length];
+            int[]check = input;
+            for(int i = 0; i < input.Length; i++)
+            {
+                int Value = check[i];
+                if (Array.IndexOf(temp, Value) > -1 && !result.Contains(Value))
+                {
+
+                    result.Add(Value);
+                }
+                temp[i] = check[i];
+                result.ToList().ForEach(Console.WriteLine);
+            }
+
+            return result.ToArray();
+        }
     }
 }
